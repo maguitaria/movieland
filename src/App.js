8e3252d5a347124d23a478e8c9c521478fc7195a
maguitaria <t2glma00@students.oamk.fi>
2023-09-15 17:04:24 +0300

    Accomplished small app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-import MovieCard from "./MovieCard";
+import MovieCard from "./MovieCard.jsx";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
 const API_URL = "http://www.omdbapi.com?apikey=d97d3ff1";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState('') // search value is a string
+  const [movies, setMovies] = useState([]); // movies are an array fetched from API
 
   useEffect(() => {
     searchMovies("Batman");
@@ -17,14 +18,14 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
+console.log(data)
     setMovies(data.Search);
   };
 
   return (
     <div className="app">
       <h1>MovieLand</h1>
-
+      <h2 style={{color: "white"}}>Best Movies of your choice!</h2>
       <div className="search">
         <input
           value={searchTerm}
@@ -41,7 +42,7 @@ const App = () => {
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard movie={movie} key={movie.imdbID} />
           ))}
         </div>
       ) : (
