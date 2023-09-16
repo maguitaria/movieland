31e9e25f3991b8a3816b5b8763823bab259fbd9d
maguitaria <t2glma00@students.oamk.fi>
2023-09-16 15:06:59 +0300

    Changed APi endpoint

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard.jsx";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=d97d3ff1";
+const API_URL = "https://www.omdbapi.com?apikey=d97d3ff1";
 
 const App = () => {
 
