1fb60220f3dcc83a865ca6d08b459f9e7bfd613c
maguitaria <t2glma00@students.oamk.fi>
2023-10-13 15:42:57 +0300

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {  Link, HashRouter, Routes, Route } from "react-router-dom";
 import MovieList from "./pages/MovieList.jsx";
 import "./css/App.css";
 import MovieDetail from "./pages/MovieDetail.jsx";
@@ -8,23 +8,35 @@ import Signup from "./pages/SignupPage.jsx"
 import Login from "./pages/LoginPage.jsx"
 import Logout from "./pages/Logout.jsx"
 
+
 const App = () => {
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <h2 style={{ color: "white" }}>Best Movies of your choice!</h2>
-
-      <Router>
+      <HashRouter basename="/">
+        <ul>
+          <li>
+            <Link to="/login"> Login</Link>
+          </li>
+          <br />
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+        <Logout />
         Navbar
         <LeftNavbar />
-        <Logout />
         <Routes>
-          <Route path="/movieland" element={<MovieList />} />
-          <Route path="/movieland/movie/:id" element={<MovieDetail />} />
-          <Route path="/movieland/signup" element={<Signup />} />
-          <Route path="/movieland/login" element={<Login />} />
+          <Route path="/" element={<MovieList />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 };
